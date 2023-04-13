979251fb93206dbb7d75f6472e7bb0141244884a
KhaiYuan <94588522+KhaiYuan@users.noreply.github.com>
2023-04-13 10:46:42 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cipjj3t0hftu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheahkhaiyuan-employee"
+customregion = "us-east-1"
